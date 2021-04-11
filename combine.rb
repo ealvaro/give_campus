50f4a8768e4f47d0adf8c87d8a677ef09ac9d3c3
ealvaro <alvaroescobaronline@gmail.com>
2021-04-10 20:44:14 -0400

    Updated the ruby script with rubocopy suggestions

--- combine.rb
+++ combine.rb
@@ -1,7 +1,9 @@
-#Input
+# Input
+# frozen_string_literal: true
+
 # files offline-donors.csv & online-donors.csv
-#
-#Output
+
+# Output
 # [
 #  {
 #  name: "Area of Greatest Need",
@@ -10,42 +12,41 @@
 #  }
 #
 # ]
+
 require 'csv'
 require 'json'
 
 grp = Hash.new(0)
 cnt = Hash.new(0)
 
-def load_offline(grp,cnt)
-
-  CSV.open("offline-donors.csv", headers: true).each do |line|
-    name =  line["designation_name"]
+def load_offline(grp, cnt)
+  CSV.open('offline-donors.csv', headers: true).each do |line|
+    name = line['designation_name']
     unless name.nil? || name.empty?
-      grp[name] += line["amount"].to_f
+      grp[name] += line['amount'].to_f
       cnt[name] += 1
     end
   end
 end
 
-def load_online(grp,cnt)
+def load_online(grp, cnt)
+  CSV.open('online-donors.csv', headers: true).each do |line|
+    des = JSON.parse line['designation']
+    next if des.empty?
 
-  CSV.open("online-donors.csv", headers: true).each do |line|
-    des = JSON.parse line["designation"]
-    unless des.empty?
-      des.each { |g|
-        name = g[0]
-        grp[name] += g[1].to_f
-        cnt[name] += 1
-      }
+    des.each do |g|
+      name = g[0]
+      grp[name] += g[1].to_f
+      cnt[name] += 1
     end
   end
 end
 
-load_offline(grp,cnt)
-load_online(grp,cnt)
+load_offline(grp, cnt)
+load_online(grp, cnt)
 
 out = []
-grp.each {|g|
-  out << { name: g[0], donors: cnt[g[0]], dollars: g[1]} unless g[0].nil?
-}
+grp.each do |g|
+  out << { name: g[0], donors: cnt[g[0]], dollars: g[1] } unless g[0].nil?
+end
 puts out.to_json
